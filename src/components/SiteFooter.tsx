676103875ef216732dec3e371c7e9b96ffbbbfeb
Jahid Hasan Milon <jahidhasanmilon999@gmail.com>
2026-08-27 00:01:35 +0600

    Add attribution footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,7 +1,6 @@
 export default function SiteFooter() {
   return (
     <footer className="site-footer">
-      <p>This is an unofficial app and is not affiliated with the German Embassy.</p>
       <p>Created and developed by Jahid Hasan Milon.</p>
     </footer>
   );
